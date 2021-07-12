b97c99f58d28072ee4f6aaf7a917fc25b23f362e
wangbolun300 <wangbolun@buaa.edu.cn>
2021-07-12 21:30:11 +0800

    not finish run heap on gpu

--- src/queue.h
+++ src/queue.h
@@ -19,29 +19,29 @@ public:
 	//MinHeap(int capacity);
     MinHeap();
 	// to heapify a subtree with the root at given index
-	void MinHeapify(int);
+	__device__ void MinHeapify(int);
 
 	// index
-	int parent(int i) { return (i - 1) / 2; }
+	__device__ int parent(int i) { return (i - 1) / 2; }
 
 	// to get index of left child of node at index i
-	int left(int i) { return (2 * i + 1); }
+	__device__ int left(int i) { return (2 * i + 1); }
 
 	// to get index of right child of node at index i
-	int right(int i) { return (2 * i + 2); }
+	__device__ int right(int i) { return (2 * i + 2); }
 
 	// to extract the root which is the minimum element
-	item extractMin();
+	__device__ item extractMin();
 
 	// Decreases key value of key at index i to new_val
-	void decreaseKey(int i, item new_val);
+	__device__ void decreaseKey(int i, item new_val);
 
 	// Returns the minimum key (key at root) from min heap
-	item getMin() { return harr[0]; }
+	__device__ item getMin() { return harr[0]; }
 
 	// Deletes a key stored at index i
-	void deleteKey(int i);
+	__device__ void deleteKey(int i);
 
 	// Inserts a new key 'k'
-	void insertKey(item k);
+	__device__ void insertKey(item k);
 };
