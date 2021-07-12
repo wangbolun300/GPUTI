c6459c8a5798eedfd91bf170d9d93cb04606271a
wangbolun300 <wangbolun@buaa.edu.cn>
2021-07-12 13:39:16 +0000

    .

--- app/main.cu
+++ app/main.cu
@@ -8,18 +8,20 @@
 
 
 int main(int argc, char ** argv){
-    MinHeap h;
-    h.insertKey(item(3));
-	h.insertKey(item(2));
-	h.deleteKey(1);
-	h.insertKey(item(15));
-	h.insertKey(item(5));
-	h.insertKey(item(4));
-	h.insertKey(item(45));
-    std::cout << h.extractMin().key << " ";
-    std::cout << h.getMin().key << " ";
-    h.decreaseKey(2, 1);
-    std::cout << h.getMin().key;
+    // MinHeap h;
+    // h.insertKey(item(3));
+	// h.insertKey(item(2));
+	// h.deleteKey(1);
+	// h.insertKey(item(15));
+	// h.insertKey(item(5));
+	// h.insertKey(item(4));
+	// h.insertKey(item(45));
+    // std::cout << h.extractMin().key << " ";
+    // std::cout << h.getMin().key << " ";
+    // h.decreaseKey(2, 1);
+    // std::cout << h.getMin().key;
+    std::cout<<"size "<<sizeof(MinHeap)<<std::endl;
+
     std::cout<<"done!"<<std::endl;
     return 1;
 }
