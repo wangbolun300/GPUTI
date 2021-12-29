ae1377331451480d5fe37668d1abc54350bb2ed6
dbelgrod <davidbelgrod@gmail.com>
2021-12-28 22:00:59 -0500

    helper functions for ccdgpu

--- src/root_finder.h
+++ src/root_finder.h
@@ -11,4 +11,10 @@ __device__ void vertexFaceMinimumSeparationCCD(const CCDdata &data_in,const CCDC
 __device__ void edgeEdgeMinimumSeparationCCD(const CCDdata &data_in,const CCDConfig& config, CCDOut& out);
 void run_memory_pool_ccd(const std::vector<std::array<std::array<Scalar, 3>, 8>> &V, bool is_edge,
                  std::vector<bool> &result_list, int parallel_nbr, double &runtime);
+
+// can be removed once device-only run_memory_pool_ccd copied over
+__global__ void initialize_memory_pool(MP_unit *units, int query_size);
+__global__ void compute_vf_tolerance_memory_pool(CCDdata *data, CCDConfig *config, const int query_size);
+__global__ void shift_queue_pointers(CCDConfig * config);
+__global__ void vf_ccd_memory_pool(MP_unit *units, int query_size, CCDdata *data, CCDConfig *config, int *results);               
 }
