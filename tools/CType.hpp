625d3fb694c8068e215a3b2ca4fb064c004b4274
dbelgrod <davidbelgrod@gmail.com>
2022-01-06 19:30:23 -0500

    adding  dtypes (to convert broad to narrow)

--- tools/CType.hpp
+++ tools/CType.hpp
@@ -1,15 +1,24 @@
 #pragma once
 #include <cfloat>
+#include <cuda.h>
+#include <cuda_runtime.h>
 
-namespace ccd{
+namespace ccd
+{
 #ifdef GPUTI_USE_DOUBLE_PRECISION
-typedef double Scalar;
-#warning using double
+	typedef double3 Scalar3;
+	typedef double2 Scalar2;
+	typedef double Scalar;
+#warning Using Double
 #define SCALAR_LIMIT DBL_MAX;
 #else
-typedef float Scalar; 
-#warning using float
+	typedef float3 Scalar3;
+	typedef float2 Scalar2;
+	typedef float Scalar;
+#warning Using Float
+#define make_Scalar3 make_float3
+#define make_Scalar2 make_float2
 #define SCALAR_LIMIT INT_MAX;
 #endif
 
-}
+} // namespace ccd
